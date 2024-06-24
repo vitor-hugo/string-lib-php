a00cdb886a0a261f7f87ad3201dde5977e5557e6
vitor-hugo <vitor83dev@gmail.com>
2024-06-24 13:45:13 -0300

    initial commit

---
+++ src/TString.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\TString;
+
+/**
+ * Small toolset for validating and manipulating strings.
+ *
+ * @link https://github.com/vitor-hugo/string-lib-php
+ */
+class TString
+{
+}
