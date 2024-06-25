a78fce2ead01bef191bfdb54fd012997dfa1be10
vitor-hugo <vitor83dev@gmail.com>
2024-06-25 10:19:43 -0300

    init: minLength validator

---
+++ src/Traits/Validators/TStringMinLength.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\TString\Traits\Validators;
+
+trait TStringMinLength
+{
+    /**
+     * Validates if the length of a string is greater than or equal to a minimum parameter.
+     *
+     * @param string $str String to be validated.
+     * @param int $min Minimum acceptable length. Must be >= 0.
+     * @return bool
+     */
+    protected function minLength(string $str, int $min): bool
+    {
+        $len = mb_strlen($str);
+        $min = $min < 1 ? 1 : $min;
+        return $len >= $min;
+    }
+}
