09834451da6e6fdf0ee15443649217b03f96f171
vitor-hugo <vitor83dev@gmail.com>
2024-07-01 16:36:39 -0300

    init: options for IsUrl validator

---
+++ src/Options/UrlOptions.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Torugo\TString\Options;
+
+class UrlOptions
+{
+    public array $protocols = ["http", "https", "ftp"];
+    public bool $requireTld = true;
+    public bool $requireProtocol = false;
+    public bool $requireHost = true;
+    public bool $requirePort = false;
+    public bool $requireValidProtocol = true;
+    public bool $allowUnderscores = false;
+    public bool $allowTrailingDot = false;
+    public bool $allowProtocolRelativeUrls = false;
+    public bool $allowFragments = true;
+    public bool $allowQueryComponents = true;
+    public bool $allowAuth = true;
+    public bool $allowNumericTld = false;
+    public bool $allowWildcard = false;
+    public bool $validateLength = true;
+    public bool $ignoreMaxLength = false;
+}
